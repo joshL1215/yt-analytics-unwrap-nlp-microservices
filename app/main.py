e1618ad6437bafac807e19b22e1db45652502c84
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-10-25 06:50:18 -0700

    -

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-from api.routes import routes
 from fastapi.middleware.cors import CORSMiddleware
-
-# TODO: make it pre load ML models
+from app.api.routes import router
 
 app = FastAPI()
 
-app.include_router(routes.router, prefix="/api")
+origins = [
+    "http://localhost",
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],   
+    allow_headers=["*"], 
+)
+
+app.include_router(router, prefix="/api")
